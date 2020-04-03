77f702421cec55ad3ee0b47670cb541919d7c1cd
Desai <shivan.s.desai@gmail.com>
2020-04-02 23:47:43 -0400

    First Commit

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
-
+const generateMarkdown = require("./utils/generateMarkdown")
 // Require all npm packages and files
 
 const questions = [
+
+    {
+        type: "input",
+        message: "What is your Github Username?",
+        name: "username"
+    },
+
+    {
+        type: "input",
+        message: "Web Development Journey",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Description",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "License",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Contributing",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Tests",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Questions",
+        name: "questions"
+    },
+    {
+        type: "input",
+        message: "Tests",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Profile Picture",
+        name: "profilepicture"
+    },
+
 // questions to user using "enquirer"
 ];
 
 function init() {
 
+    inquirer
+    .prompt(questions)
+    .then(answers => {
+        console.log(answers)
+        axios.get("api.github.com/users/Stationgamesd95/"+ answers.username)
+        .then( data => {
+
+
+
+            generateMarkdown(response)
+
+        })
+    })
+
 // Logic for this app shoud be here. You can create more functions if you like. This is just a template for you to start your Homework but it is not required for you to use it.
+
+
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,57 @@
+
+
 function generateMarkdown(data) {
   return `
-# ${data.title}
+  #  ${data.title}
+  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+  ## Description
+
+  Welcome to the starting your Web Development Journey. You want to learn to create a website, to do so there are requirements you have to meet in order to begin.
+
+
+  ## Installation
+  
+    Download Visual Code (https://code.visualstudio.com)
+    Sign Up for Git Hub (https//github.com)
+  
+  
+  ## Usage 
+  
+  Provide instructions and examples for use. Include screenshots as needed. 
+  
+  
+  ## Credits
+  
+  Shivan Desai and the UCF WEB DEVELOPMENT BOOTCAMP Team. (https://github.com/UCF-Coding-Boot-Camp/UCF-ORL-FSF-FT-03-2020-U-C)
+  
+  
+  ## License
+  
+  GNU GPLv3
+  
+  ---
+  
+  
+  ## Contributing
+  
+  Thank the team who has been contrbuting: https://github.com/UCF-Coding-Boot-Camp/UCF-ORL-FSF-FT-03-2020-U-C
+
+  ## Profile Picture
+
+  ![Test Image 1]("https://avatars2.githubusercontent.com/u/47505249?v=4")
+  
+  ## Tests
+  
+  To test it, open an HTML file in Visual Studio Code and press Ctrl+Shift+B . It should open in Chrome.
+  
+  
+  ---
+  © 2020 Desai Web Company, a Web Development Market, Inc. brand. All Rights Reserved.
+
 
 `;
 }
+
 
 module.exports = generateMarkdown;
